1c54df266d87ea92b9c603ea0eb761ed343a0d74
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-21 13:02:25 -0700

    Test outputting toml

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-const core = require("@actions/core");
+import * as core from "@actions/core";
 // const github = require("@actions/github");
+import * as toml from "toml";
+import * as fs from "node:fs";
 
 try {
   const command = core.getInput("command");
+  const tomlPath = core.getInput("toml-path");
 
   console.log(`Hello from action: ${command}`);
+
+  if (tomlPath) {
+    const output = toml.parse(fs.readFileSync(tomlPath, "utf-8"));
+
+    console.log("--- what we got", output)
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
