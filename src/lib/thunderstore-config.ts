cc242a95398ef8e6cbfc7dec5d20b2ea468b6f71
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-21 13:26:53 -0700

    Add test manifest generation

---
+++ src/lib/thunderstore-config.ts
@@ -0,0 +1,33 @@
+import * as fs from "node:fs";
+import * as core from "@actions/core";
+import * as toml from "toml";
+
+export interface ThunderstoreConfig {
+  config: { schemaVersion: string; }
+  package: {
+    namespace: string;
+    name: string;
+    versionNumber: string;
+    description: string;
+    websiteUrl: string;
+    containsNsfwContent: boolean;
+    dependencies: Record<string, string>;
+  }
+  publish: {
+    repository: string;
+    communities: string[];
+    categories: string[];
+  }
+}
+
+export const getConfig = () => {
+  const tomlPath = core.getInput("toml-path");
+
+  if (tomlPath) {
+    const output: ThunderstoreConfig = toml.parse(fs.readFileSync(tomlPath, "utf-8"));
+
+    return output;
+  }
+
+  return undefined;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,35 @@
 import * as core from "@actions/core";
 // const github = require("@actions/github");
-import * as toml from "toml";
-import * as fs from "node:fs";
+import { getConfig } from "./lib/thunderstore-config";
 
 try {
   const command = core.getInput("command");
-  const tomlPath = core.getInput("toml-path");
 
   console.log(`Hello from action: ${command}`);
 
-  if (tomlPath) {
-    const output = toml.parse(fs.readFileSync(tomlPath, "utf-8"));
+  switch (command) {
+    case "sync": {
+      const config = getConfig();
 
-    console.log("--- what we got", output)
+      const manifest = {};
+      manifest["name"] = config.package.name;
+      manifest["version_number"] = config.package.versionNumber;
+      manifest["website_url"] = config.package.websiteUrl;
+      manifest["description"] = config.package.description;
+      manifest["dependencies"] = [];
+
+      for (const [id, version] of Object.entries(config.package.dependencies)) {
+        manifest["dependencies"].push(`${id}-${version}`);
+      }
+
+      console.log("--- manifest would be:", JSON.stringify(manifest, null, 2));
+
+      break;
+    }
+
+    default:
+      console.log(`Usage: ${command}`);
+      break;
   }
 } catch (error) {
   core.setFailed(error.message);
