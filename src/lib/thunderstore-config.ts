ccd225886dbdb8eb79e179b84e8e89b16dfa55d5
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-21 13:27:12 -0700

    Run formatter

--- src/lib/thunderstore-config.ts
+++ src/lib/thunderstore-config.ts
@@ -3,7 +3,7 @@ import * as core from "@actions/core";
 import * as toml from "toml";
 
 export interface ThunderstoreConfig {
-  config: { schemaVersion: string; }
+  config: { schemaVersion: string };
   package: {
     namespace: string;
     name: string;
@@ -12,22 +12,24 @@ export interface ThunderstoreConfig {
     websiteUrl: string;
     containsNsfwContent: boolean;
     dependencies: Record<string, string>;
-  }
+  };
   publish: {
     repository: string;
     communities: string[];
     categories: string[];
-  }
+  };
 }
 
 export const getConfig = () => {
   const tomlPath = core.getInput("toml-path");
 
   if (tomlPath) {
-    const output: ThunderstoreConfig = toml.parse(fs.readFileSync(tomlPath, "utf-8"));
+    const output: ThunderstoreConfig = toml.parse(
+      fs.readFileSync(tomlPath, "utf-8")
+    );
 
     return output;
   }
 
   return undefined;
-}
+};
