7d9136e8a8d31addc123e9acf71facc360e9633b
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-28 14:12:18 -0700

    Move generate manifest to it's own file

--- src/cmd/generate-manifest.ts
+++ src/cmd/generate-manifest.ts
@@ -1,1 +1,27 @@
-export default () => {};
+import * as fs from "node:fs";
+import { getConfig } from "../lib/thunderstore-config";
+import * as path from "node:path";
+import * as core from "@actions/core";
+
+export default () => {
+  const config = getConfig();
+  const tomlPath = core.getInput("toml-path");
+
+  const manifest = {};
+  manifest["name"] = config.package.name;
+  manifest["version_number"] = config.package.versionNumber;
+  manifest["website_url"] = config.package.websiteUrl;
+  manifest["description"] = config.package.description;
+  manifest["dependencies"] = [];
+
+  for (const [id, version] of Object.entries(config.package.dependencies)) {
+    manifest["dependencies"].push(`${id}-${version}`);
+  }
+
+  const manifestData = JSON.stringify(manifest, null, 2);
+    const filename = path.join(path.dirname(tomlPath), "manifest.json");
+
+  fs.writeFileSync(filename, manifestData);
+
+  core.info(`Wrote manifest: ${filename}`);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,21 @@
 import * as core from "@actions/core";
 // const github = require("@actions/github");
-import { getConfig } from "./lib/thunderstore-config";
+import generateManifest from "./cmd/generate-manifest";
 
 try {
-  const command = core.getInput("command");
+  const [command, subcommand] = core.getInput("command").split(" ");
 
   console.log(`Hello from action: ${command}`);
 
   switch (command) {
-    case "sync": {
-      const config = getConfig();
-
-      const manifest = {};
-      manifest["name"] = config.package.name;
-      manifest["version_number"] = config.package.versionNumber;
-      manifest["website_url"] = config.package.websiteUrl;
-      manifest["description"] = config.package.description;
-      manifest["dependencies"] = [];
-
-      for (const [id, version] of Object.entries(config.package.dependencies)) {
-        manifest["dependencies"].push(`${id}-${version}`);
-      }
-
-      console.log("--- manifest would be:", JSON.stringify(manifest, null, 2));
-
+    /** Install dependencies */
+    case "install":
       break;
-    }
+
+    /** Generate and write the manifest.json */
+    case "gen-manifest":
+      generateManifest();
+      break;
 
     default:
       console.log(`Usage: ${command}`);
